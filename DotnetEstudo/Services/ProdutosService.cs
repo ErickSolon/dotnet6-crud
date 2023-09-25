68e20da0dc7a954061969338fd2c0047fee05112
Érick Solon <erick.solon.prog@gmail.com>
2023-09-24 23:05:04 -0300

    .

--- DotnetEstudo/Services/ProdutosService.cs
+++ DotnetEstudo/Services/ProdutosService.cs
@@ -21,12 +21,10 @@ namespace DotnetEstudo.Services
             try
             {
                 var Produto = _context.Produtos.SingleOrDefault(x => x.Id == id);
-
                 if (Produto == null)
                 {
                     return NotFound("Produto não existe, nada foi excluído!");
                 }
-
                 _logger.LogInformation($"Dado deletado! {id}");
                 _context.Produtos.Remove(Produto);
                 _context.SaveChanges();
@@ -34,7 +32,6 @@ namespace DotnetEstudo.Services
             {
                 _logger.LogError($"Erro na classe: {nameof(DeletebyId)}, StackTrace: {e.StackTrace}");
             }
-
             return NoContent();
         }
 
@@ -46,21 +43,16 @@ namespace DotnetEstudo.Services
                 {
                     return BadRequest("Valores inválidos para 'page' ou 'size'.");
                 }
-
                 var skipCount = (page - 1) * size;
-
                 var Resultado = _context.Produtos
                     .Skip(skipCount)
                     .Take(size)
                     .ToList();
-
                 if (Resultado == null || Resultado.Count == 0)
                 {
                     return Problem("Não foi encontrado nenhum dado!");
                 }
-
                 List<ProdutosDTO> produtosDTOs = new();
-
                 foreach (var item in Resultado)
                 {
                     produtosDTOs.Add(new ProdutosDTO
@@ -69,42 +61,35 @@ namespace DotnetEstudo.Services
                         Titulo = item.Titulo
                     });
                 }
-
                 _logger.LogInformation($"Total de dados em uma requisição: {produtosDTOs.Count.ToString()}");
                 return Ok(produtosDTOs);
             } catch(Exception e)
             {
                 _logger.LogError($"Erro na classe: {nameof(GetAll)}, StackTrace: {e.StackTrace}");
             }
-
             return NoContent();
         }
-
 
         public IActionResult GetById(int id)
         {
             try
             {
                 var Resultado = _context.Produtos.SingleOrDefault(x => x.Id == id);
-
                 if (Resultado == null)
                 {
                     return NotFound("Dados sobre o produto não foram encontrados!");
                 }
-
                 var produtosDTOs = new ProdutosDTO
                 {
                     Id = Resultado.Id,
                     Titulo = Resultado.Titulo
                 };
-
                 _logger.LogInformation($"Id do dado requisitado: {id}");
                 return Ok(produtosDTOs);
             } catch(Exception e)
             {
                 _logger.LogError($"Erro na classe: {nameof(GetById)}, StackTrace: {e.StackTrace}");
             }
-
             return NoContent();
         }
 
@@ -116,19 +101,15 @@ namespace DotnetEstudo.Services
                 {
                     return Problem("Digite os campos para salvar os dados!");
                 }
-
                 _context.Produtos.Add(produtos);
                 _context.SaveChanges();
-
                 _logger.LogInformation($"Dado salvo: {produtos.Id}");
                 return CreatedAtAction(nameof(GetById), new { id = produtos.Id }, produtos);
             } catch(Exception e)
             {
                 _logger.LogError($"Erro na classe: {nameof(SaveProduto)}, StackTrace: {e.StackTrace}");
             }
-
             return NoContent();
-            
         }
 
         public IActionResult UpdateById(int id, Produtos produtos)
@@ -136,29 +117,23 @@ namespace DotnetEstudo.Services
             try
             {
                 var Produto = _context.Produtos.SingleOrDefault(x => x.Id == id);
-
                 if (produtos.Id == 0)
                 {
                     return Problem("Id não pode ser 0!");
                 }
-
                 if (Produto == null)
                 {
                     return NotFound("Produto inexistente! nada foi atualizado!");
                 }
-
                 Produto.Titulo = produtos.Titulo;
                 Produto.QtdVendida = produtos.QtdVendida;
-
                 _context.SaveChanges();
-
                 _logger.LogInformation($"Dado atualizado! {id}");
                 return Ok(Produto);
             } catch(Exception e)
             {
                 _logger.LogError($"Erro na classe: {nameof(UpdateById)}, StackTrace: {e.StackTrace}");
             }
-
             return NoContent();
         }
     }
