134a14e88a2f080461b97bd5fac92a52511e5b73
Érick Solon <erick.solon.prog@gmail.com>
2023-10-14 06:37:21 -0300

    ExportData adicionado

---
+++ ExportData/Program.cs
@@ -0,0 +1,23 @@
+﻿using EstudoRepositories.Models.DTOs;
+using ExportData.Utils;
+using Newtonsoft.Json;
+
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        using (var Http = new HttpClient())
+        {
+            var Response = await Http.GetStringAsync("https://localhost:7042/api/Detran");
+            List<MultadosDTO> Result = JsonConvert.DeserializeObject<List<MultadosDTO>>(Response);
+
+            var pdf = new PDFWriter();
+            pdf.PDFWrite(Result);
+
+            Console.WriteLine($"Dados adicionados, Total de itens: {Result.Count}");
+            
+        }
+
+        await Task.Delay(2000);
+    }
+}
---
+++ ExportData/Utils/PDFWriter.cs
@@ -0,0 +1,58 @@
+﻿using EstudoRepositories.Models.DTOs;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace ExportData.Utils;
+public class PDFWriter
+{
+    public PDFWriter()
+    {
+
+    }
+
+    public void PDFWrite(List<MultadosDTO> infos)
+    {
+        QuestPDF.Settings.License = LicenseType.Community;
+
+        Document.Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Margin(2, Unit.Centimetre);
+                page.Background(Colors.White);
+                page.DefaultTextStyle(x => x.FontSize(11));
+
+                page.Header()
+                    .Text("Pessoas Multadas!")
+                    .SemiBold().FontSize(25).FontColor(Colors.Green.Medium);
+
+                page.Content()
+                .PaddingVertical(1, Unit.Centimetre)
+                .Column(x =>
+                {
+                    foreach (var item in infos)
+                    {
+                        x.Item().Text($"\n\nId: {item.Id}");
+                        x.Item().Text($"Marca: {item.Marca}");
+                        x.Item().Text($"MultaStatus: {item.MultaStatus}");
+                        x.Item().Text($"VeiculoId: {item.VeiculoId}");
+                        x.Item().Text($"CPF: {item.CPF}");
+                        x.Item().Text($"NomeCompleto: {item.NomeCompleto}");
+                        x.Item().Text($"Placa: {item.Placa}\n\n");
+                    }
+                });
+
+                page.Footer()
+                    .AlignCenter()
+                    .Text(x =>
+                    {
+                        x.Span($"Total de pessoas multadas: {infos.Count}\n");
+                        x.CurrentPageNumber();
+                    });
+            });
+        })
+        .GeneratePdf($"Multados-{DateTime.Now.ToString(@"dd-MM-yyyy")}.pdf");
+    }
+}
